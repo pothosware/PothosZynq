fe5b3c421f83b2839fb50187abe540220cd7b1b8
Josh Blum <josh@joshknows.com>
2015-01-22 15:19:11 -0800

    pothos-zynq: added missing stdio include

--- driver/pothos_zynq_dma_driver.h
+++ driver/pothos_zynq_dma_driver.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2014 Josh Blum
+// Copyright (c) 2014-2015 Josh Blum
 // SPDX-License-Identifier: BSL-1.0
 
 /***********************************************************************
@@ -9,6 +9,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 //! Return error codes
 #define PZDUD_OK 0
